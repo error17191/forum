b436ec825b2414e10ad0189127a6b22bd4c75f63
Mohamed Ahmed <error17191@gmail.com>
2018-05-25 21:28:48 +0200

    Ep004 Done

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Reply;
 use App\Thread;
 use App\User;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -15,15 +16,26 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function unauthenticated_user_can_not_add_replies()
+    {
+        $this->expectException(AuthenticationException::class);
+        $thread = factory(Thread::class)->create();
+
+        $reply = factory(Reply::class)->make();
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
+    }
+
+    /** @test */
     public function an_authenticated_user_can_participate_in_forum_threads()
     {
         $this->be($user = factory(User::class)->create());
 
         $thread = factory(Thread::class)->create();
 
-        $reply = factory(Reply::class)->create();
+        $reply = factory(Reply::class)->make();
 
-        $this->post('/threads/' . $thread->id . '/replies', $reply->toArray());
+        $this->post($thread->path() . '/replies', $reply->toArray());
 
         $this->get($thread->path())->assertSee($reply->body);
     }
